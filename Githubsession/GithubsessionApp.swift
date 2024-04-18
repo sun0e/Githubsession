1bc83cd39996e2b2bffb4eec58c331954e5bc0a1
Sun0e <kindyoung2@gmail.com>
2024-04-18 20:23:13 +0900

    Initial Commit

---
+++ Githubsession/GithubsessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubsessionApp.swift
+//  Githubsession
+//
+//  Created by Sunyoung Jeon  on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubsessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
